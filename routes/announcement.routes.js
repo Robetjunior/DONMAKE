8a6e8d88175b9730eb73ec76e953812cc596244d
Erick-34 <erickoliveira2060@gmail.com>
2020-07-31 15:05:51 -0300

    route

---
+++ routes/announcement.routes.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const router = express.Router();
+
+const Announcement = require('../models/Announcement.model');
+
+
+//POST/ Create Announcement
+router.post("/announcement/create", async (req, res) => {
+    const { title, description, imgPath, value } = req.body;
+    try {
+        if (!title || !description || !imgPath || !value) {
+            res.status(400).json({ message: "Please provide all informations" });
+        }
+        const response = await Announcement.create({
+            title,
+            description,
+            imgPath: req.file.url,
+            value,
+            adId: req.session.currentUser._id
+        });
+
+        return res.status(201).json(response);
+    }
+    catch (err) {
+        console.log(`Error while creating a new  announcement ${err}`)
+    }
+});
+
+
+//GET/Announcement list
+router.get('/announcement', async (req, res) => {
+    try {
+        const response = await Announcement.find()
+
+        return res.status(200).json(response)
+    }
+    catch (err) { }
+});
+
+
+//GET/details Announcement 
+router.get("/announcement/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const response = await Announcement.findOne(id);
+        res.status(200).json(response);
+
+    } catch (err) { }
+});
+
+
+//PATCH/Update Announcement
+
+router.patch("/announcement/:id/edit", async (req, res) => {
+    const { title, description, value, imgPath } = req.body;
+    const { id } = req.params;
+
+    try {
+        if (!title || !description || !value || !imgPath) {
+            return res
+                .status(400)
+                .json({ message: "Please provide all informations" });
+        }
+
+        const result = await Announcement.updateOne({ _id: id }, req.body);
+
+        res.status(200).json(result);
+    } catch (err) {
+        throw new Error(err);
+    }
+});
+
+//DELETE/ Delete Announcement
+
+router.delete("/announcement/:id/delete", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const response = await Announcement.deleteOne({ id });
+
+        res.status(200)
+    } catch (err) {
+        throw new Error(err);
+    }
+})
